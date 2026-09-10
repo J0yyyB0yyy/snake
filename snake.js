f52ab3fbc47ca0d674690dfc0e6c1640ff6a6658
J0yyyB0yyy <mirj4995@gmail.com>
2026-09-10 16:09:31 +0530

    snake with basic movement.

--- snake.js
+++ snake.js
@@ -1,57 +1,127 @@
-let bgColor={r: 132, g: 0, b: 255};
-let x=0, y=0;
-let direction="";
+let bgColor = { r: 132, g: 0, b: 255 };
+let direction = "", moveDirection ="";
+let gameOver = false;
+let lastMove=0, moveDelay=200;
 
+let snake = [
+    [300, 300],
+    [280, 300],
+    [260, 300]
+];
 
-class food{
-
-};
-class Snake{
-
-};
-
-
-function setup(){
+function setup() {
     createCanvas(600, 600);
-    background(bgColor.r, bgColor.g, bgColor.b);
 }
 
+function move(snake, direction) {
 
-function draw(){
-    
-    if(direction==="up"){
-        y=y-2;
+    if (direction === "up") {
+        snake.unshift([
+            snake[0][0],
+            snake[0][1] - 20
+        ]);
+        snake.pop();
     }
-    else if(direction === "down"){
-        y=y+2;
+
+    else if (direction === "down") {
+        snake.unshift([
+            snake[0][0],
+            snake[0][1] + 20
+        ]);
+        snake.pop();
     }
-    else if(direction === "right"){
-        x=x+2;
+
+    else if (direction === "right") {
+        snake.unshift([
+            snake[0][0] + 20,
+            snake[0][1]
+        ]);
+        snake.pop();
     }
-    else if(direction === "left"){
-        x=x-2;
+
+    else if (direction === "left") {
+        snake.unshift([
+            snake[0][0] - 20,
+            snake[0][1]
+        ]);
+        snake.pop();
     }
-    
-    background(bgColor.r, bgColor.g, bgColor.b);
-    fill(0,0,0)
-    square(x, y, 20);
-    
-    
 }
 
-function keyPressed(){
-    if(keyCode===UP_ARROW || key==='w'){
-        direction="up";
+function draw() {
+
+    background(bgColor.r, bgColor.g, bgColor.b);
+
+    // border
+    stroke(0, 0, 0);
+    strokeWeight(10);
+    noFill();
+    rect(0, 0, width, height);
+
+    // Move snake
+    if (!gameOver && direction !== "") {
+        if(millis()-lastMove>=moveDelay){
+            move(snake, direction);
+            lastMove=millis();
+        }
+        
     }
-    else if(keyCode===DOWN_ARROW || key==='s'){
-        direction="down";
+
+    // Check collision
+    if (
+        snake[0][0] + 20 > width - 10 ||
+        snake[0][0] < 10 ||
+        snake[0][1] + 20 > height - 10 ||
+        snake[0][1] < 10
+    ) {
+        gameOver = true;
     }
-    else if(keyCode===LEFT_ARROW || key==='a'){
-        direction="left";
+
+    //snake
+    fill(0, 0, 0);
+
+    for (let val of snake) {
+        square(val[0], val[1], 20);
     }
-    else if(keyCode===RIGHT_ARROW || key==='d'){
-        direction="right";
+
+    // Game over
+    if (gameOver) {
+        fill(255, 0, 0);
+        textSize(50);
+        textAlign(CENTER, CENTER);
+        text("GAME OVER", width / 2, height / 2);
     }
+}
+
+function keyPressed() {
+
+    if (gameOver === true) {
+        return;
+    }
+
+    if (keyCode === UP_ARROW || key === "w") {
+        if(direction !="down"){
+            direction = "up";
+        }
+    }
+
+    else if (keyCode === DOWN_ARROW || key === "s") {
+        if(direction !="up"){
+            direction = "down";
+        }
+    }
+
+    else if (keyCode === LEFT_ARROW || key === "a") {
+        if(direction !="right"){
+            direction = "left";
+        }
+    }
+
+    else if (keyCode === RIGHT_ARROW || key === "d") {
+        if(direction !="left"){
+            direction = "right";
+        }
+    }
+
     return false;
-    
 }
